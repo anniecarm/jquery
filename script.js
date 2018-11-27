72a5e451f3d134da90cc37235785008b5f260641
Annie Carmichael <42788203+anniecarm@users.noreply.github.com>
2018-11-27 12:44:05 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+$(function() {
+  //All JS goes within these brackets  
+  
+  $("#age-submit").click(function(){
+    
+    //select the box with id of age
+    //get the value from that box
+    var ageString = $("#age").val();
+
+    //convert string to number
+    var age = parseInt(ageString);
+
+    if ( age <= 1996 ) {
+      $("answer").text("hi millenial!");
+      $("#neil-form").show();
+      $("#age-form").hide();
+    } else {
+      $("#answer").text("nope. not a millenial");
+      $("#club-img").attr("src", "http://energy.gov/sites/prod/files/styles/borealis_default_hero_respondlarge/public/door_5481543.jpg?itok=joaNZ-Zx");
+    }
+    
+  });
+
+  $("#neil-submit").click(function(){
+    var neilAlbumsString = $("#neil-albums").val();
+    var neilAlbums = parseInt(neilAlbumsString);
+
+    if ( neilAlbums > 5 ) {
+      $("#neil-form").hide();
+      $("#answer").text("hello millenail!");
+      $("#club-img").attr("src", "https://media.giphy.com/media/zyxiqEQb6wTSg/giphy.gif");
+      
+    } else {
+      $("#answer").text("nope. not a millenal.");
+      $("#club-img").attr("src", "http://energy.gov/sites/prod/files/styles/borealis_default_hero_respondlarge/public/door_5481543.jpg?itok=joaNZ-Zx");
+    }
+  });
+
+});
+
+
+
